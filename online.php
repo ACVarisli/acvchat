0107683da96f9fe78a7a2008d4dcab3c40e18a85
Ahmet Can VARIŞLI <acvprod2014@outlook.com>
2016-09-17 04:08:45 -0700

    Online kullanıcıların bulunması (sql üzerinden)

---
+++ online.php
@@ -0,0 +1,14 @@
+<?php
+    include "config/linkmysql.php";
+	$_SESSION["uyeadi"] = $kullanici;
+
+  $time = time();
+  $oturum_ekle = mysql_query("UPDATE uyeler SET sonetkinlik='$time' WHERE uyeadi='$kullanici'");
+  $time_check = time()-30;
+  $sorgu = mysql_query("SELECT* FROM uyeler WHERE  sonetkinlik > '$time_check'");
+    while ( $diz = mysql_fetch_array($sorgu)){
+        echo "<div class='onlineuser'> ";
+          echo $diz['uyeadi']; echo "<i class='fa fa-circle' style='color: #16a085; margin-right: 10px;'></i>  </div>";
+        }
+
+?>
